09e8493132ed8b7cfbb0d85ecb73623a5afe13c3
Antony Nyagah <antonynyagah@antonys-MBP.attlocal.net>
2019-03-23 14:01:56 -0400

    tab commit

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 
 /*iniial commit */
 
@@ -10,10 +11,35 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.tabs.map(tab => (
+            <Tab key={tab} 
+            selectTabHandler={props.selectTabHandler}
+            selectedTab={props.selectedTab} 
+            tab={tab} />
+         ))}  
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
-export default Tabs;
+
+Tabs.propTypes = {
+    tabs: PropTypes.shape({
+      headline: PropTypes.string.isRequired,
+      img: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired, 
+      type: PropTypes.arrayOf( 
+        PropTypes.oneOf([
+          'all ',
+          'javascript ',
+          'technology ',
+          'node ',
+          'React.js ',
+          'bootstrap ',
+        ])
+      )
+    })
+  };
+
+export default Tabs; 
