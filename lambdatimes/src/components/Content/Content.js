c5af2bf6c382460bc0a95ca5face2687cbcd7276
Antony Nyagah <antonynyagah@antonys-MBP.attlocal.net>
2019-03-23 14:53:24 -0400

    cards and content commit

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,12 +18,19 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    });
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-  };
-
+  return () => {
+    this.setState({ 
+      selected: tab}); 
+     }
+    };
   filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
@@ -37,8 +44,13 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
-  };
+     if (this.state.selected === 'all') {
+      return this.state.cards
+    }
+      return this.state.cards.filter(card => card.tab.includes(this.state.selected))
+  }
+
+ 
 
   render() {
     return (
@@ -48,7 +60,10 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} /> 
+        <Tabs tabs={this.state.tabs} 
+        selectedTab={this.state.selected}
+        selectTabHandler={this.state.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
