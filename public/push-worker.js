17576567f4b987e37f60c3a4ba740b861af0bc6e
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2020-12-27 14:50:12 -0600

    updated index and push worker again

--- public/push-worker.js
+++ public/push-worker.js
@@ -1,9 +1,9 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1608740954.317088
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1609101975.2109556
 importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://tony\u002Dnyagah\u002Dportfolio.herokuapp.com/static/media/IMG_7710.3f5e8b09.png',
-  defaultTitle: 'Antony\u0027s Test',
-  defaultActionURL: 'https://antony-nyagah-lambdatimes.herokuapp.com/',
+  defaultTitle: 'Tony\u0027s React App',
+  defaultActionURL: 'https://antony\u002Dnyagah\u002Dlambdatimes.herokuapp.com/',
   appKey: 'zNLA9iRVRWSAugoWcR8bgw',
   token: 'MTp6TkxBOWlSVlJXU0F1Z29XY1I4Ymd3OlhSUFpkM05ISWNfb1ZoU2pwMFNUSUZoYk5PTGR4ZkZPMDlXTkYtZjBoYUE',
   vapidPublicKey: 'BAjT-2hQmRfQm4PojOiQLPQfTjsoOhWbumskxEd1VU_gMfcX_kGKBW6dzKq3_6D7fWIqXNjvSbAJhpepJsRoiKQ='
